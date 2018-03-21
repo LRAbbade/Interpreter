c41731839d723aecffdc5d9e81acfb7251ff78f7
Lucas Abbade <lucasabbade@hotmail.com>
2018-03-20 23:08:54 -0300

    adding form to page

--- frontend/frontend/frontend.py
+++ frontend/frontend/frontend.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return render_template('page.html')
+
+# dar um jeito de pegar a action do form
+@app.route('/run?<int:instruction>&<int:register_1>&<int:register_2>')
+def show_post(instruction, register_1, register_2):
+    print(instruction, register_1, register_2)
