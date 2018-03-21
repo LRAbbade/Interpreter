535ec243d1f37a609cd651b1936ec449df2cee6c
Lucas Abbade <lucasabbade@hotmail.com>
2018-03-21 14:04:32 -0300

    get and post requests added

--- frontend/frontend/frontend.py
+++ frontend/frontend/frontend.py
@@ -1,12 +1,17 @@
-from flask import render_template
-from flask import Flask
+from flask import render_template, Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return render_template('page.html')
 
-# dar um jeito de pegar a action do form
-@app.route('/run?<int:instruction>&<int:register_1>&<int:register_2>')
-def show_post(instruction, register_1, register_2):
-    print(instruction, register_1, register_2)
+# exemplo de como pegar as infos do form e postar em outro html
+# arrumar agora pra usar o comando em main.py e printar o resultado em run.html
+@app.route('/run', methods=['POST'])
+def show_post():
+    context = {
+        'instruction':request.form['instruction'],
+        'register_1':request.form['register_1'],
+        'register_2':request.form['register_2']
+    }
+    return render_template('run.html', **context)
